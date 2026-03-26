08754476e61c348102ad17c0492e82a3dd78b3ee
Leonardo Bonino <boninoleonardo@gmail.com>
2026-03-26 19:02:19 +0000

    Ajout d'une configuration pour la connection à la bdd distante

--- bdd/bdd.php
+++ bdd/bdd.php
@@ -1,11 +1,10 @@
 <?php
 
+require_once ROOT . '/config.php';
+
 try{
 
-    $user='root';
-    $mdp= '';
-
-    $bdd= new PDO("mysql:host=localhost;dbname=medinfo",$user, $mdp);
+    $bdd= new PDO("mysql:host=$host;dbname=$dbname",$user, $mdp);
 
 }catch(PDOException $e){
     print "Erreur de connexion :".$e->getMessage()."<br>";
